e59f01dac7f0fbfacbc9d437aebeeacaa126bb94
Aniruddha Shastri <aniruddha.shastri@gmail.com>
2026-02-15 20:43:49 -0600

    Upgrade to ESLint 9 flat config and remove deprecated lint deps

---
+++ backend/eslint.config.js
@@ -0,0 +1,25 @@
+const tsParser = require('@typescript-eslint/parser');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+module.exports = [
+  {
+    ignores: ['dist/**']
+  },
+  {
+    files: ['src/**/*.ts', 'test/**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules
+    }
+  }
+];
